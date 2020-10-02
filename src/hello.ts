eeb9a9c789aea57fad9141cce4568a7fac9d1f3d
Derrick S <69398796+DStone93@users.noreply.github.com>
2020-10-01 17:16:10 -0700

    Initial commit

---
+++ src/hello.ts
@@ -0,0 +1,9 @@
+export const hello = () => "Hello world!";
+
+// export function hello(){
+//     return 'Hello world!';
+// }
+
+// const hello = function(){
+//     return 'Hello World!'
+// }
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { hello } from "./hello";
+
+const message = hello();
+console.log(message);
