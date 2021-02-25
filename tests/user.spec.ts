e381668b2d5ffa9ec6357d56d643bcf83ca78c1b
Khatab AL-Daghistani <kal-daghistani16510@alumni.focuscollege.com>
2021-02-24 18:35:49 -0800

    outline test design

---
+++ tests/user.spec.ts
@@ -0,0 +1,14 @@
+// import { user } from "../src/classes/user";
+// import { expect } from "chai";
+// import "mocha";
+
+// describe("User test", () => {
+//     it("should create user", () => {
+//         const result = user();
+//         expect(result).to.equal("Hello world!");
+//     });
+//     it("should delete user", () => {
+//         const result = user();
+//         expect(result).to.equal("Hello world!");
+//     });
+// });
