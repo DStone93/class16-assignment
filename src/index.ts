765cde5bac765ce3b2f09c07dc064598d4a7ee5a
Derrick <dstone16640@alumni.focuscollege.com>
2021-02-24 20:31:49 -0800

    Modified write to FS for article use

--- src/index.ts
+++ src/index.ts
@@ -18,3 +18,14 @@ testUser2.loadByUsername("user")
 console.log(testLogin.loginUser("user"))
 
 const loadUser2 = testUser2.loadByUsername("user")
+
+console.log(/**** */)
+const art1 = new ArticleBuilder();
+art1.setTitle("Godzilla")
+art1.setId()
+art1.setType(eArticle.Guest)
+art1.setDescription("desc")
+art1.setBody("body")
+art1.publishArticle();
+
+console.log(art1)
