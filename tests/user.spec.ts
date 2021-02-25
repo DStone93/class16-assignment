b5d8786ea912d2ccf58e0ffa62c4faa799d1728a
Khatab AL-Daghistani <kal-daghistani16510@alumni.focuscollege.com>
2021-02-24 18:45:45 -0800

    add test user, waiting for finished code

--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -1,14 +1,16 @@
-// import { user } from "../src/classes/user";
-// import { expect } from "chai";
-// import "mocha";
+import { User } from "../src/classes/user";
+import { expect } from "chai";
+import "mocha";
 
-// describe("User test", () => {
-//     it("should create user", () => {
-//         const result = user();
-//         expect(result).to.equal("Hello world!");
-//     });
-//     it("should delete user", () => {
-//         const result = user();
-//         expect(result).to.equal("Hello world!");
-//     });
-// });
+describe("User test", () => {
+    it("should create user", () => {
+        const user = new User();
+        // const createUser = user.createUser(user)
+
+        expect(user).to.equal("Hello world!");
+    });
+    it("should delete user", () => {
+        const user = new User().loginUser();
+        expect(user).to.equal("Hello world!");
+    });
+});
